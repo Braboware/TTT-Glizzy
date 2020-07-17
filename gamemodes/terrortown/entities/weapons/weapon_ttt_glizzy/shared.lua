d102aa817ed4b1732652bb2d13aafea6437bfb82
Kurt <kurtkyle@gmail.com>
2020-07-17 18:47:27 -0400

    removed comment

--- gamemodes/terrortown/entities/weapons/weapon_ttt_glizzy/shared.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_glizzy/shared.lua
@@ -207,7 +207,6 @@ function SWEP:ThrowGlizzy( model_file )
     local mass = 10 -- min mass that still allows water splash at reasonable angles
     
     if supershot then
-        -- velocity = ( 34800/(20.1-self:Clip1()) - (1/20) )  -- visualize in desmos; self:Clip1() is x
         velocity = ( 348000/(20.1-self:Clip1()) - (327900/20) )  -- visualize in desmos; self:Clip1() is x
         mass = ( 10/(20.245-self:Clip1()) - (1/2) ) + mass
         recoil = math.pow(self:Clip1(), 1.125) -- relative oomph
